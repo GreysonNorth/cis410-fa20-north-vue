e0f8ea3e976d6cf9cff1337f8bf687bfba847a15
GreysonNorth <grey@rams.colostate.edu>
2020-12-16 18:43:20 -0700

    15.2 header, logout, /account gaurd

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,15 @@ import NotFound from './components/NotFound.vue';
 import ReviewCreate from './components/reviewCreate.vue';
 import SignUp from './components/signUp.vue';
 import Orders from './components/orders.vue';
-
+import store from './store.js';
 Vue.use(VueRouter);
 
 const routes = [
 {path:'/',component: Home},
-{path:'/account',component: Account},
+{path:'/account',component: Account, beforeEnter(to, from, next){
+    if(store.state.token){next()}
+    else{next('/signin')}
+}},
 {path:'/signin',component: Login},
 {path:'/products',component: Products},
 {path: '/products', component: Orders},
