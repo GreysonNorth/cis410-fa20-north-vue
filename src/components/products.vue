36f00f9c303f73a26ade1cc5129979392bca91b6
GreysonNorth <grey@rams.colostate.edu>
2020-12-16 17:04:16 -0700

    15.2 products

--- src/components/products.vue
+++ src/components/products.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
       <h1>Products</h1>  
+
+      <table class="table">
+        <thead>
+          <tr>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Category Name</th>
+              <th> </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-form="thisProduct in products" :key="thisProduct.ProductID">
+            <th>{{thisProduct.productName}}</th>
+            <td>{{thisProduct.productPrice}}</td>
+            <td>{{thisProduct.CategoryName}}</td>
+            <td>
+              <router-link :to="`/products/${thisProduct.ProductID}`">
+              <button type="button" class="btn btn-primary">Details</button>
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed:{
+      products(){return this.$store.state.products}
+    }
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import axios from 'axios';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -15,9 +15,19 @@ export default new Vuex.Store({
         },
         storeUserInApp(state, myUser){
             state.user = myUser
+        },
+        storeProducts(state, myProducts){
+            state.products = myProducts
         }
     },
     actions:{
-
+        getProducts({commit}){
+            axios.get(`/products`)
+            .then((myResponse)=>{
+                console.log("response from getProducts action", myResponse)
+                commit('storeProducts', myResponse.data)
+            })
+            .catch(()=>{console.log("error in getProducts")})
+        }
     }
 })
